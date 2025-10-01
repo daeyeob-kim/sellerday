119ca9c921408a72164e402907a85012aed45956
Kim Daeyeob <ongoz@naver.com>
2025-10-02 02:12:19 +0900

    deploy option

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # config/deploy/production.rb
-server '139.59.114.156', user: 'root', roles: %w{web app db}
+server '139.59.114.156', user: 'deployer', roles: %w{web app db}
 
 set :rails_env, :production
 set :branch, :main # 프로덕션 배포에 사용할 브랜치
